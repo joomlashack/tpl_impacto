e6315eff2cfdf48f81e6ea365da28d18934c8ab5
gnomeontherun <gnomation@gnomeontherun.com>
2011-02-11 19:56:41 +0000

    Updating ponzi code system wide, linking to common version

--- trunk/js/template.css.php
+++
@@ -1,7 +0,0 @@
-<?php
-// no direct access
-defined( '_JEXEC' ) or die( 'Restricted index access' );
-?>
-<?php
-echo "<a href=\"http://www.joomlashack.com\" title=\"Joomla Templates\">Joomla Templates</a>";
-?>
